3db943e1ad918bf78c969431becd145c316b265e
M Banks <montensia1@outlook.com>
2019-06-03 18:16:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   project = kickstarter.css("li.project.grid_4").first
   # projects: kickstarter.css("li.project.grid_4")
-  #
+  # project.css("h2.bbcard_name strong a").text
   binding.pry
 end
 
