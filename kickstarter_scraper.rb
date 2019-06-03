07bbddcd14f5ece1afac7b0fd2855e0d52564bc0
M Banks <montensia1@outlook.com>
2019-06-03 18:31:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,8 +7,8 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  project = {}
-  project = kickstarter.css("li.project.grid_4").first
+  projects = {}
+  # project = kickstarter.css("li.project.grid_4").first
   # projects: kickstarter.css("li.project.grid_4")
   # project.css("h2.bbcard_name strong a").text
   # project.css("div.project-thumbnail a img").attribute("src").value
