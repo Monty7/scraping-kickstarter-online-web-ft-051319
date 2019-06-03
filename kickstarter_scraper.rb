4a122c33d4911fa24a22e159da68dd0fc53f128b
M Banks <montensia1@outlook.com>
2019-06-03 18:16:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,7 @@ def create_project_hash
   project = kickstarter.css("li.project.grid_4").first
   # projects: kickstarter.css("li.project.grid_4")
   # project.css("h2.bbcard_name strong a").text
+  # project.css("div.project-thumbnail a img").attribute("src").value
   binding.pry
 end
 
